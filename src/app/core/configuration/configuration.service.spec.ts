65ac58966667fb88220e1ed98228bc264a80b895
Touré Holder <toureholder@gmail.com>
2020-07-16 21:13:23 -0300

    refactor: create config api

--- src/app/core/configuration/configuration.service.spec.ts
+++ src/app/core/configuration/configuration.service.spec.ts
@@ -4,22 +4,25 @@ import getConfigApiResponse from '../../../testing/api-responses/get-config.json
 import { ConfigurationService } from './configuration.service';
 import { of } from 'rxjs';
 import { ConfigurationCacheService } from './cache/configuration-cache.service';
+import { ConfigurationApiService } from './api/configuration-api.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ConfigurationService', () => {
   let service: ConfigurationService;
-  let mockHttpClient: jasmine.SpyObj<HttpClient>;
+  let mockApi: jasmine.SpyObj<ConfigurationApiService>;
   let mockCache: jasmine.SpyObj<ConfigurationCacheService>;
 
   beforeEach(() => {
-    mockHttpClient = jasmine.createSpyObj('HttpClient', ['get']);
+    mockApi = jasmine.createSpyObj('ConfigurationApiService', ['fetchConfig']);
     mockCache = jasmine.createSpyObj('ConfigurationCacheService', [
       'getConfiguration',
       'setConfiguration',
     ]);
 
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [
-        { provide: HttpClient, useValue: mockHttpClient },
+        { provide: ConfigurationApiService, useValue: mockApi },
         { provide: ConfigurationCacheService, useValue: mockCache },
       ],
     });
@@ -30,23 +33,9 @@ describe('ConfigurationService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should fetch config from api', () => {
-    // Given
-    const apiResponse = getConfigApiResponse;
-    mockHttpClient.get.and.returnValue(of(apiResponse));
-
-    // When
-    const configObservable = service.getConfig();
-
-    // Then
-    configObservable.subscribe((data) => {
-      expect(data).toEqual(jasmine.objectContaining(apiResponse));
-    });
-  });
-
   it('#getConfig should get check cache', () => {
     // Given
-    mockHttpClient.get.and.returnValue(of(getConfigApiResponse));
+    mockApi.fetchConfig.and.returnValue(of(getConfigApiResponse));
 
     // When
     service.getConfig();
@@ -57,7 +46,7 @@ describe('ConfigurationService', () => {
 
   it('should get config from cache if available', () => {
     // Given
-    mockHttpClient.get.and.returnValue(of(getConfigApiResponse));
+    mockApi.fetchConfig.and.returnValue(of(getConfigApiResponse));
 
     mockCache.getConfiguration.and.returnValue({ foo: 'bar' });
 
@@ -73,7 +62,7 @@ describe('ConfigurationService', () => {
   it('should get config from api if cache is not available', () => {
     // Given
     const apiResponse = getConfigApiResponse;
-    mockHttpClient.get.and.returnValue(of(apiResponse));
+    mockApi.fetchConfig.and.returnValue(of(getConfigApiResponse));
 
     mockCache.getConfiguration.and.returnValue(undefined);
 
@@ -89,7 +78,7 @@ describe('ConfigurationService', () => {
   it('should set config in cache after api response', () => {
     // Given
     const apiResponse = getConfigApiResponse;
-    mockHttpClient.get.and.returnValue(of(apiResponse));
+    mockApi.fetchConfig.and.returnValue(of(getConfigApiResponse));
 
     mockCache.getConfiguration.and.returnValue(undefined);
 
--- src/app/core/configuration/configuration.service.ts
+++ src/app/core/configuration/configuration.service.ts
@@ -3,13 +3,14 @@ import { Observable, of, config } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { ConfigurationCacheService } from './cache/configuration-cache.service';
+import { ConfigurationApiService } from './api/configuration-api.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfigurationService {
   constructor(
-    private http: HttpClient,
+    private api: ConfigurationApiService,
     private cache: ConfigurationCacheService
   ) {}
 
@@ -18,14 +19,10 @@ export class ConfigurationService {
 
     return cacheResponse
       ? of(cacheResponse)
-      : this.fetchConfigFromApi().pipe(
+      : this.api.fetchConfig().pipe(
           tap((val) => {
             this.cache.setConfiguration(val);
           })
         );
-  }
-
-  private fetchConfigFromApi(): Observable<any> {
-    return this.http.get('http://localhost:3000/config');
   }
 }
